989210eff1289063c197ed7f0a481c852217aa8a
Nurul <nurulrachmawati70@gmail.com>
2020-05-02 23:08:56 +0800

    input dengan berbagai macam komponen

---
+++ PraktekInput/backup/unit1.pas
@@ -0,0 +1,76 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btnSimpannnnn: TButton;
+    btnReset: TButton;
+    cbKota: TComboBox;
+    cbxFoto: TCheckBox;
+    cbxKTP: TCheckBox;
+    cbxIjazah: TCheckBox;
+    Edit1: TEdit;
+    Memo1: TMemo;
+    rgJenisKelamin: TRadioGroup;
+    procedure btnResetClick(Sender: TObject);
+    procedure btnSimpannnnnClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btnSimpannnnnClick(Sender: TObject);
+begin
+  Memo1.Lines.Add(Edit1.Text);
+
+  if(rgJenisKelamin.ItemIndex=0) then
+  begin
+    memo1.lines.add('Laki-laki');
+  end
+  else if(rgJenisKelamin.ItemIndex=1) then
+  begin
+    memo1.lines.add('Perempuan');
+  end;
+
+  Memo1.Lines.Add(cbKota.Text);
+
+  if(cbxFoto.Checked) then
+   memo1.Lines.Add('Foto ada');
+
+  if(cbxKTP.Checked) then
+   memo1.Lines.Add('KTP ada');
+
+  if(cbxIjazah.Checked) then
+   memo1.Lines.Add('Ijazah ada');
+end;
+
+procedure TForm1.btnResetClick(Sender: TObject);
+begin
+  Edit1.text:='';
+  Memo1.Clear;
+  rgJenisKelamin.ItemIndex:=-1;
+  cbKota.ItemIndex:=-1;
+  cbKota.Text:='';
+end;
+
+end.
+
